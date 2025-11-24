96166ff62b68114799a3fdf855c6d260f3989e62
Kirill Sak <k.sak@vkteam.ru>
2025-11-24 22:32:25 +0300

    Lab3 first try

--- Lab3/Core/Src/main.c
+++ Lab3/Core/Src/main.c
@@ -40,6 +40,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
 
@@ -48,7 +51,11 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_TIM4_Init(void);
+void RainbowFromPotentiometer(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -85,7 +92,15 @@ int main(void)
 
   /* Initialize all configured peripherals */
   /* USER CODE BEGIN 2 */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
 
+  HAL_ADC_Start(&hadc1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);  // PA7 (D11, Blue)
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);  // PB8 (D9, Red)
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);  // PB9 (D10, Green)
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -95,6 +110,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    RainbowFromPotentiometer();
+    HAL_Delay(5);
   }
   /* USER CODE END 3 */
 }
@@ -136,6 +153,183 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* PA0 - analog input (ADC1_IN0, A0, potentiometer) */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* PA7 - TIM3_CH2 (D11, Blue) */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* PB8, PB9 - TIM4_CH3, TIM4_CH4 (D9 Red, D10 Green) */
+  GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+static void MX_ADC1_Init(void)
+{
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  __HAL_RCC_ADC1_CLK_ENABLE();
+
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfig.Channel = ADC_CHANNEL_0;                     // PA0
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+#ifdef ADC_CR2_CAL
+  HAL_ADCEx_Calibration_Start(&hadc1);
+#endif
+}
+
+static void MX_TIM3_Init(void)
+{
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  __HAL_RCC_TIM3_CLK_ENABLE();
+
+  htim3.Instance = TIM3;
+  /* For 8 MHz system clock:
+   * Prescaler = 79  -> 8 MHz / (79+1) = 100 kHz
+   * Period    = 255 -> 100 kHz / 256 ≈ 390 Hz PWM
+   */
+  htim3.Init.Prescaler = 79;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 255;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_TIM4_Init(void)
+{
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  __HAL_RCC_TIM4_CLK_ENABLE();
+
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 79;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 255;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void RainbowFromPotentiometer(void)
+{
+  uint32_t adcValue;
+
+  if (HAL_ADC_PollForConversion(&hadc1, 10) != HAL_OK)
+  {
+    return;
+  }
+
+  adcValue = HAL_ADC_GetValue(&hadc1);   // 0..4095
+
+  /* Map 0..4095 -> color wheel 0..(6*256-1) */
+  uint32_t wheel = (adcValue * 6U * 256U) / 4095U;
+  uint8_t segment = wheel / 256U;       // 0..5
+  uint8_t offset  = wheel % 256U;       // 0..255
+
+  uint8_t r = 0, g = 0, b = 0;
+
+  switch (segment)
+  {
+    case 0: // Red -> Yellow
+      r = 255;
+      g = offset;
+      b = 0;
+      break;
+    case 1: // Yellow -> Green
+      r = 255 - offset;
+      g = 255;
+      b = 0;
+      break;
+    case 2: // Green -> Cyan
+      r = 0;
+      g = 255;
+      b = offset;
+      break;
+    case 3: // Cyan -> Blue
+      r = 0;
+      g = 255 - offset;
+      b = 255;
+      break;
+    case 4: // Blue -> Magenta
+      r = offset;
+      g = 0;
+      b = 255;
+      break;
+    default: // Magenta -> Red
+      r = 255;
+      g = 0;
+      b = 255 - offset;
+      break;
+  }
+
+  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, r);  // Red (PB8, D9)
+  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, g);  // Green (PB9, D10)
+  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, b);  // Blue (PA7, D11)
+}
 
 /* USER CODE END 4 */
 
